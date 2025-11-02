ac4ccc5c7fa6e602ada3a6e52da324b47c8163bc
adamkow <adamkow8@hotmail.com>
2025-11-02 22:55:59 +0000

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "exports",
-}
+  output: 'export',
+  basePath: '/sheet-music',
+  assetPrefix: '/sheet-music',
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
